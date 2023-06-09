f3197004b3621212c59908c049916c7c2d9315cf
Paramveer <paramsingh1205@outlook.com>
2023-06-09 12:31:11 +0530

    Implemented bug fixes found by Devansh-baghel

---
+++ Operations.js
@@ -0,0 +1,17 @@
+// Operations Module
+
+function addAll(firstOperand, secondOperand) {
+  return +firstOperand + +secondOperand;
+}
+
+function subtractAll(firstOperand, secondOperand) {
+  return +firstOperand - +secondOperand;
+}
+
+function multiplyAll(firstOperand, secondOperand) {
+  return +firstOperand * +secondOperand;
+}
+
+function divideAll(firstOperand, secondOperand) {
+  return +firstOperand / +secondOperand;
+}
--- script.js
+++ script.js
@@ -22,7 +22,9 @@ function watch() {
     num.addEventListener("click", (e) => {
       if (calcDisplay.innerText === "0") {
         calcDisplay.innerText = e.target.innerText;
-      } else if (calcDisplay.innerText.length < 11) {
+      } else if (
+        !(e.target.innerText === "." && calcDisplay.innerText.includes("."))
+      ) {
         calcDisplay.innerText += e.target.innerText;
       }
     });
@@ -30,8 +32,6 @@ function watch() {
 }
 
 watch();
-
-// Main Operations
 
 plus.addEventListener("click", () => {
   firstOperand = calcDisplay.innerText;
@@ -69,8 +69,20 @@ clear.addEventListener("click", () => {
 });
 
 equal.addEventListener("click", () => {
+  // Issues Regarding user input
+  if (calcDisplay.innerText == decimal.innerText) {
+    alert("Cannot add decimal after an operator");
+    calcDisplay.innerText = 0;
+  }
+
+  if ((calcDisplay.innerText.match(/\./g) || []).length > 1) {
+    alert("Invalid input: More than one decimal point.");
+    calcDisplay.innerText = 0;
+  }
+
   secondOperand = calcDisplay.innerText;
   resultDisplay.innerText += secondOperand;
+
   function operate(operator, firstOperand, secondOperand) {
     if (operator == "+") {
       calcDisplay.innerText = addAll(firstOperand, secondOperand);
@@ -82,27 +94,11 @@ equal.addEventListener("click", () => {
       calcDisplay.innerText = divideAll(firstOperand, secondOperand);
     }
   }
+
+  // operate function call
   operate(operator, firstOperand, secondOperand);
   operator = "";
   firstOperand = "";
   secondOperand = "";
   resultDisplay.innerText = "";
 });
-
-// Operations Module
-
-function addAll(firstOperand, secondOperand) {
-  return +firstOperand + +secondOperand;
-}
-
-function subtractAll(firstOperand, secondOperand) {
-  return +firstOperand - +secondOperand;
-}
-
-function multiplyAll(firstOperand, secondOperand) {
-  return +firstOperand * +secondOperand;
-}
-
-function divideAll(firstOperand, secondOperand) {
-  return +firstOperand / +secondOperand;
-}
